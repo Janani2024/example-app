0902e1042d28487a629df5d0570f204b4df70ca4
JANANI <jananiad2@gmail.com>
2023-02-01 10:50:11 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+ <head>
+    <title> Resume</title>
+</head>
+<body>
+    <h1 style="text-align: center">Resume</h1>
+    <style>
+        div {
+          margin: 100px;
+         }
+        </style>
+    <div>
+        <h3 style=color:black> CAREER OBJECTIVE</h3>
+        <p> &nbsp&nbsp&nbsp&nbsp&nbsp&nbspTo became a successful professional in the field of information technology by utikizing my skills to achive
+            goals <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspof a company that focus on customer satisfaction.</P>
+            <hr>
+        <p><b style=color:black> NAME :</b> Janani</p>
+        <p><b style=color:black>E-MAIL :</b> jananict8@gmail.com</p>
+        <hr>
+    <h3 style=color:black>Education History</h3>
+    <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp2020-2024 Pursing B.TECH Artificial Intelligence and Data Science third year in Sri Shanmugha college of Engineering and technology with 8.54 CGPA </p>
+    <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp2019-2020 completed 12th standard in Government Higher Secondary school Sowdhapuram with 74%</p>
+    <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp2017-2018 completed 10th standard in Government Higher Secondary school Sowdhapuram with 84%</p>
+    <hr>
+    <h3 style=color:black>SKILLS</h3>
+    <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspPython</p>
+    <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspHTML(Basics)</p></div>
